20613a3ac0ab52cc7c0f1f162cd91e31a5793419
Heaven <mc5223@student.uni-lj.si>
2021-05-17 19:53:03 +0200

    Added Izdelki.

--- src/sample/HubController.java
+++ src/sample/HubController.java
@@ -1,4 +1,40 @@
 package sample;
 
-public class HubController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HubController implements Initializable {
+    public ListView lvDisplay;
+    public TableView tvTabela;
+    public TextField tfCena;
+    public Button bDodaj;
+    public Spinner sKolicina;
+    public Button bNaroci;
+    public TableView tvTabela1;
+    public TableColumn<Izdelek, String> imeIzdelkaColumn;
+    public TableColumn<Izdelek, Integer>  zalogaColumn;
+    public TableColumn<Izdelek, Integer>  cenaColumn;
+    private ObservableList<Izdelek> izdelekData = FXCollections.observableArrayList();
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        izdelekData.add(new Izdelek("iPhone 12", 260, 600));
+        izdelekData.add(new Izdelek("iPhone X", 100, 400));
+        izdelekData.add(new Izdelek("iPhone SE", 40, 500));
+        izdelekData.add(new Izdelek("Samsung Galaxy S12", 35, 900));
+        izdelekData.add(new Izdelek("PS5", 14, 499));
+        imeIzdelkaColumn.setCellValueFactory(cellData -> cellData.getValue().imeIzdelkaProperty());
+        zalogaColumn.setCellValueFactory(cellData -> cellData.getValue().zalogaIzdelkaProperty().asObject());
+        cenaColumn.setCellValueFactory(cellData -> cellData.getValue().cenaIzdelkaProperty().asObject());
+        tvTabela1.setItems(getIzdelekData());
+    }
+
+    public ObservableList<Izdelek> getIzdelekData() {
+        return izdelekData;
+    }
 }
---
+++ src/sample/Izdelek.java
@@ -0,0 +1,64 @@
+package sample;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+import java.time.LocalDate;
+
+public class Izdelek {
+    private final StringProperty imeIzdelka;
+    private final IntegerProperty zalogaIzdelka;
+    private final IntegerProperty cenaIzdelka;
+
+
+
+    public Izdelek() {
+        this(null, 0, 0);
+    }
+
+
+    public Izdelek(String imeIzdelka, Integer zalogaIzdelka, Integer cenaIzdelka) {
+        this.imeIzdelka = new SimpleStringProperty(imeIzdelka);
+        this.zalogaIzdelka = new SimpleIntegerProperty(zalogaIzdelka);
+        this.cenaIzdelka = new SimpleIntegerProperty(cenaIzdelka);
+    }
+
+    public String getImeIzdelka() {
+        return imeIzdelka.get();
+    }
+
+    public void setImeIzdelka(String imeIzdelka) {
+        this.imeIzdelka.set(imeIzdelka);
+    }
+
+    public StringProperty imeIzdelkaProperty() {
+        return imeIzdelka;
+    }
+
+    public int getZalogaIzdelka() {
+        return zalogaIzdelka.get();
+    }
+
+    public void setZalogaIzdelka(Integer zalogaIzdelka) {
+        this.zalogaIzdelka.set(zalogaIzdelka);
+    }
+
+    public IntegerProperty zalogaIzdelkaProperty() {
+        return zalogaIzdelka;
+    }
+
+    public int getCenaIzdelka() {
+        return cenaIzdelka.get();
+    }
+
+    public void setCenaIzdelka(Integer cenaIzdelka) {
+        this.cenaIzdelka.set(cenaIzdelka);
+    }
+
+    public IntegerProperty cenaIzdelkaProperty() {
+        return cenaIzdelka;
+    }
+
+}
